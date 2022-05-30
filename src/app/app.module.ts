5849120125b6675b406b7f28ed71e69a064bdcc5
FranckJudes <franckjudes87@gmail.com>
2022-05-30 06:21:57 +0100

    Le Front-End
    front-End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CompetenceComponent } from './competence/competence.component';
+import { ScolaireComponent } from './scolaire/scolaire.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ProposComponent } from './propos/propos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CompetenceComponent,
+    ScolaireComponent,
+    ProfilComponent,
+    ProposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'navbar', component: NavbarComponent},
+      {path: 'competence', component: CompetenceComponent},
+      {path: 'scolaire', component: ScolaireComponent},
+      {path: 'profil', component: ProfilComponent},
+      {path: 'propos', component: ProposComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
