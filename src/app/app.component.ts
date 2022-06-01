83056a983f2b598bb7ad60e306f09152617df6ce
darylmbeng <106031446+darylmbeng@users.noreply.github.com>
2022-06-01 09:11:12 +0100

    pic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,38 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    
-  }
-  constructor(){
+  selectedFile: File = null;
 
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('', fd)
+    .subscribe(res => {
+      console.log(res);
+    })
   }
 }
+
+form: FormGroup ;
+  color = 'cyan';
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      theme: this.color
+    })
+    
+    this.form.valueChanges.subscribe(data => {
+      this.color = data.theme
+      console.log('Form changes', data)
+    })
+  }
+
+
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
-import { RouterModule } from '@angular/router';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CompetenceComponent } from './competence/competence.component';
-import { ScolaireComponent } from './scolaire/scolaire.component';
-import { ProfilComponent } from './profil/profil.component';
-import { ProposComponent } from './propos/propos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    CompetenceComponent,
-    ScolaireComponent,
-    ProfilComponent,
-    ProposComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-      {path: 'navbar', component: NavbarComponent},
-      {path: 'competence', component: CompetenceComponent},
-      {path: 'scolaire', component: ScolaireComponent},
-      {path: 'profil', component: ProfilComponent},
-      {path: 'propos', component: ProposComponent}
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
