9ebdc5eecd39695069d67fd560fdf5c8134889d4
Bateki93 <95339199+Bateki93@users.noreply.github.com>
2022-06-10 06:55:03 +0100

    ajout

---
+++ app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CompetenceComponent } from './competence/competence.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ProposComponent } from './propos/propos.component';
+import { ScolaireComponent } from './scolaire/scolaire.component';
+import { Templet1Component } from './templet1/templet1.component';
+
+const routes: Routes = [
+  {path:'' ,redirectTo:'login', pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'register',component: RegisterComponent},
+  {path: 'navbar', component: NavbarComponent},
+   {path: 'competence', component: CompetenceComponent},
+  {path: 'scolaire', component: ScolaireComponent},
+  {path: 'profil', component: ProfilComponent},
+  {path: 'propos', component: ProposComponent},
+  {path: 'templet1', component: Templet1Component}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
